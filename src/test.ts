c478adf33bd42e2d8ec80aa52da5bd0f91841d95
Grayson Nocera <graysonnocera@Graysons-MacBook-Air-6.local>
2023-01-26 17:58:44 -0500

    demonstrate generator functions in TS

--- src/test.ts
+++ src/test.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import fs from 'fs';
 
 interface Repository {
   name: string;
--- src/test_apis.ts
+++ src/test_apis.ts
@@ -8,6 +8,13 @@
 //     return data;
 // }
 
+function* generator() {
+    console.log("Generator is starting")
+    yield 1
+    console.log("Middle of generator")
+    yield 2
+    console.log("Generator finished")
+}
 
 function calculateTax(income: number, taxYear: number): number {
     if (taxYear < 2022)
@@ -16,3 +23,11 @@ function calculateTax(income: number, taxYear: number): number {
 }
 
 calculateTax(10_000, 2022)
+let gen = generator()
+console.log(gen.next())
+console.log("Control is back with main")
+console.log(gen.next())
+
+let result = gen.next()
+console.log(result.value)
+console.log(result.done)
