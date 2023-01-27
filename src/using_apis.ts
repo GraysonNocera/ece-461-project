ac9f63bea214308b67c7cf20aed8a0a328f067d2
b4yuan <89487381+b4yuan@users.noreply.github.com>
2023-01-27 14:03:21 -0500

    adding some user input for testing

--- src/using_apis.ts
+++ src/using_apis.ts
@@ -1,4 +1,5 @@
 import { Octokit } from "octokit";
+import * as readline from 'readline';
 
 // Set up authentication token
 // To do this, I copied and pasted my github token into an environment variable
@@ -7,8 +8,29 @@ const octokit = new Octokit({
   auth: process.env.GITHUB_TOKEN,
 });
 
+let rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+rl.question('Is this example useful? [y/n] ', (answer) => {
+switch(answer.toLowerCase()) {
+    case 'y':
+    console.log('Super!');
+    break;
+    case 'n':
+    console.log('Sorry! :(');
+    break;
+    default:
+    console.log('Invalid answer!');
+}
+rl.close();
+});
+
+
+
 // Async functions must return a Promise type
-async function myFunc(): Promise<void> {
+export async function myFunc(): Promise<void> {
 
     // This call will pull 4 issues from the repo ece-461-project using the personal access
     // token above and store the result/data in the variable result
@@ -29,20 +51,20 @@ async function myFunc(): Promise<void> {
     })
 
 
-    console.log(result.data)
-    console.log(another_result.data)
+    // console.log(result.data)
+    // console.log(another_result.data)
 
     // Get pull request title
-    console.log(another_result.data["title"])
+    // console.log(another_result.data["title"])
     // console.log(result.headers)
     // console.log(result.status)
     // console.log(result.url)
 
-    console.log("\n\n\n\n\n\n" + result.data["title"])
-    
+    // console.log("\n\n\n\n\n\n" + result.data["title"])
+    let countIss = 0
     // Iterate through the issues and print their title
     for (let i = 0; i < result.data.length; i++) {
-        console.log(result.data[i]["title"])
+        countIss++ // console.log(result.data[i]["title"])
     }
 }
 
