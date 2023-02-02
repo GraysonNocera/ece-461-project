b1dd9585458f6426417241d653c5b409fda85d4e
GraysonNocera <gnocera@purdue.edu>
2023-02-01 22:57:26 -0500

    feat: add function to count lines of code with cloc

--- src/clone_repo.ts
+++ src/clone_repo.ts
@@ -1,6 +1,12 @@
 // File for functions to get information from cloned repo
 import fs from "fs";
 import path from "path";
+import cp from "child_process";
+import { parse } from "csv-parse";
+
+async function delay(ms: number) {
+    return new Promise( resolve => setTimeout(resolve, ms) );
+}
 
 function get_readme_length(path_to_repo: string): number {
     // Get length in characters of a readme file
@@ -9,8 +15,7 @@ function get_readme_length(path_to_repo: string): number {
 
     let file_contents: string;
     let file: string = path.join(path_to_repo, "README.md");
-
-    // Assume we are in the base path of the repo we just cloned
+    
     try {
         file_contents = fs.readFileSync(file, "ascii");
     } catch(exception) {
@@ -21,4 +26,28 @@ function get_readme_length(path_to_repo: string): number {
     return file_contents?.length;
 }
 
-console.log(get_readme_length("./src"))
+function get_percentage_comments(path_to_repo: string): number {
+
+    let percent = 0;
+    let src_folder: string = "\"" + path.join(path_to_repo, "src/") + "\"";
+    let data_file: string = "\"" + path.join(path_to_repo, "data.txt") + "\"";
+    let terminal_command: string = "cloc --by-percent cm --sum-one --report-file=" + data_file + " --csv " + src_folder;
+    let terminal_output = cp.exec(terminal_command, (error, stdout, stderr) => {
+        if (error) {
+          console.error(`exec error: ${error}`);
+        }
+    });
+    
+    data_file = path.join(path_to_repo, "data.txt")
+    const csv_file_content = fs.readFileSync(data_file, "ascii");
+    const csv_data = parse(csv_file_content, (err, records, info) => {
+        console.log("Error: ", err);
+    });
+    //console.log(csv_data)
+
+    return percent
+}
+
+let path_to_repo = "/Users/graysonnocera/Desktop/Spring 2023/ECE 461/ece-461-project/"
+console.log(get_readme_length(path_to_repo))
+console.log(get_percentage_comments(path_to_repo))
