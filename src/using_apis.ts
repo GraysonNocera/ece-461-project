4c01a422d2ccd23c484509820088ecd3d33b5ad9
b4yuan <89487381+b4yuan@users.noreply.github.com>
2023-02-01 20:11:54 -0500

    Fixed and added a bunch of correctness things

--- src/using_apis.ts
+++ src/using_apis.ts
@@ -1,91 +1,112 @@
-import { Octokit } from "octokit";
+// import { Octokit } from "octokit";
+import { Octokit as OctokitType } from "octokit";
+
+const Octokit = OctokitType as any;
+
 import * as readline from 'readline';
 
 // Set up authentication token
 // To do this, I copied and pasted my github token into an environment variable
 // in vs code and referenced it in this line
 const octokit = new Octokit({ 
-  auth: process.env.GITHUB_TOKEN,
+  auth: "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",
   userAgent: "using apis",
   timeZone: "Eastern",
   baseUrl: 'https://api.github.com',
 });
 
-let rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-rl.question('Is this example useful? [y/n] ', (answer) => {
-switch(answer.toLowerCase()) {
-    case 'y':
-    console.log('Super!');
-    break;
-    case 'n':
-    console.log('Sorry! :(');
-    break;
-    default:
-    console.log('Invalid answer!');
-}
-rl.close();
-});
-
-
-
-// Async functions must return a Promise type
-// <<<<<<< HEAD
-//  export async function myFunc(repos:string = "", owners:string = ""): Promise<number> {
-
-//     // This call will pull 4 issues from the repo ece-461-project using the personal access
-//     // token above and store the result/data in the variable result
-
-//     // let result = await octokit.request("GET /repos/GraysonNocera/ece-461-project/issues", {
-//     //     owner: owner,
-//     //     repo: repos,
-//     //     per_page: 4
-//     // });
-// //paginate allows us to see active issues
-//     let issues = await octokit.paginate("GET /repos/{owner}/{repo}/issues", {
-//         owner: owners,
-//         repo: repos
-//     });
-//   //  console.log(test.length);
-
-//   //can return a number this way but has to return to async or else it won't work properly 
-//     return issues.length; 
-
-//     //console.log(result.headers)
-//     // console.log(result.headers)
-//     // console.log(result.status)
-//     // console.log(result.url)
-
-//   //  console.log("\n\n\n\n\n\n" + result.data["title"])
-    
-//     //Iterate through the issues and print their title
-//     // for (let i = 0; i < test.length; i++) {
-//     //     console.log(test[i])
-//     // }
-// }
-
-// //myFunc()
-
 export async function get_issues(repo: string, owner: string): Promise<number> {
-
-    // This call will pull 4 issues from the repo ece-461-project using the personal access
-    // token above and store the result/data in the variable result
-    let issuecount = 0
-    // Get pull request #5 from our repo
-    let another_result = await octokit.request('GET /repos/{owner}/{repo}/issues{?state,head,base,sort,direction,per_page,page}',{
-        owner: owner,
-        repo: repo,
-        state: "closed",
-        since: "2023-01-23"
-    })
+    let issuecount = 0;
+    let page = 1;
+    let per_page = 30;
+    let issuesRemaining = true;
     
-    // Iterate through the issues and print their title
-    for (let i = 0; i < another_result.data.length; i++) {
-        issuecount++ // console.log(result.data[i]["title"])
+    try {
+        while (issuesRemaining) {
+            let result = await octokit.request('GET /repos/{owner}/{repo}/issues{?state,head,base,sort,direction,per_page,page}', {
+                owner: owner,
+                repo: repo,
+                state: "closed",
+                since: "1999-01-23",
+                per_page: per_page,
+                page: page
+            });
+
+            issuecount += result.data.length;
+
+            if (result.data.length < per_page) {
+                issuesRemaining = false;
+            } else {
+                page++;
+            }
+        }
+    } catch (error) {
+        console.error("Could not find number of issues in repository.");
+    }
+    
+    return issuecount;
+}
+
+export async function get_commitcount(repo: string, owner: string): Promise<number> {
+    let count = 0;
+    let page = 1;
+    let per_page = 30;
+    let commitsRemaining = true;
+
+    try {
+        while (commitsRemaining) {
+            let result = await octokit.request(
+                'GET /repos/{owner}/{repo}/commits{?sha,path,author,since,until,page,per_page}',
+                {
+                    owner: owner,
+                    repo: repo,
+                    page: page,
+                    per_page: per_page
+                }
+            );
+
+            count += result.data.length;
+
+            if (result.data.length < per_page) {
+                commitsRemaining = false;
+            } else {
+                page++;
+            }
+        }
+    } catch (error) {
+        console.error("Could not find repository commit counts.");
     }
 
-    return issuecount
+    return count;
+}
+
+export async function get_lifetime(repo: string, owner: string): Promise<string> {
+    let created = "";
+
+    try {
+        let result = await octokit.request(
+        'GET /repos/{owner}/{repo}',
+        {
+            owner: owner,
+            repo: repo
+        });
+
+        created = result.data.created_at;
+    } catch (error) {
+        console.error("Could not find repository lifetime.");
+    }
+
+    return created;
+}
+
+export async function get_tcoverage(repo: string, owner: string): Promise<number> {
+    let coverage = 0;
+
+    try {
+        console.log("Test coverage function has not been written yet.")
+    } catch (error) {
+        console.error("Could not find test coverage of the repository.");
+    }
+  
+    return coverage;
 }
