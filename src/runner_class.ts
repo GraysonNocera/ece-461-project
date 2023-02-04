e4fbd025bf3b9f7e1ce132e61fed300db3a947fc
khauersp <54869912+khauersp@users.noreply.github.com>
2023-02-04 16:24:27 -0500

    feat: redefined corectness scoring

--- src/runner_class.ts
+++ src/runner_class.ts
@@ -10,25 +10,11 @@ export class Runner {
     constructor(instance : Package){
         this.package_instance = instance;
     }
-  // constructor(URL: string = "", repo = "", owner = "github") {
-  //     this.url = URL;
-  //     this.correctness = 0;
-  //     this.bus_factor = 0; 
-  //     this.ramp_up = 0;
-  //     this.issues = 0;
-  //     this.responsiveness = 0;
-  //     this.license = 0; 
-  //     this.score = 0; 
-  //     this.repo = repo; 
-  //     this.owner = owner; 
-  // }
-
   //What do we need to call from here?  but possibly interact with whatever API's we need 
   //has to be async to allow use of await to fulfil promise made by myFunc
   //Will eventually be used to calculate correctess parameter but currently just used to test API interaction
   async calculate_correctness(){
     //this.package_instance.correctness = 0; 
-
     //needed to complete promise and return a number type 
     this.package_instance.commit_count = await get_commitcount(this.package_instance.repo, this.package_instance.owner);
     if(this.package_instance.commit_count >= 1000){
