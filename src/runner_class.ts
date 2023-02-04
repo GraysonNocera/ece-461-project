cc76a2940223f918053d9468b76ecda432b535fa
khauersp <54869912+khauersp@users.noreply.github.com>
2023-02-04 16:48:54 -0500

    feat:redefined min

--- src/runner_class.ts
+++ src/runner_class.ts
@@ -22,7 +22,7 @@ export class Runner {
     } else{
       this.package_instance.commit_count /= 1000; 
     }
-    this.package_instance.correctness = 0.5*this.package_instance.commit_count + 0.8*(this.package_instance.issues_active/this.package_instance.issues);
+    this.package_instance.correctness = Math.min(0.5*this.package_instance.commit_count + 0.8*(this.package_instance.issues_active/this.package_instance.issues, 1));
 
 }
 
