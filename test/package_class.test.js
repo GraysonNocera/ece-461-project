9d41e7b02bea0bca3f9bb2adf78733dddb7edc04
khauersp <54869912+khauersp@users.noreply.github.com>
2023-02-09 00:43:05 -0500

    chore: removed unneeded file

--- test/package_class.test.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var package_class_1 = require("../src/package_class");
-describe("test initializing Package", function () {
-    test("initialize left empty", function () {
-        var test = new package_class_1.Package();
-        expect(test.bus_factor).toBe(0);
-    });
-});
