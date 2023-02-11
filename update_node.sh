3da6fc670d5d0fa5760b762399002e8023027675
Shantanu Sinha <shantanu.p.sinha@gmail.com>
2023-02-11 13:30:57 -0500

    CLI Updates Node Version properly

--- update_node.sh
+++ update_node.sh
@@ -5,6 +5,7 @@
 # Install nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
+echo $HOME/.nvm
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
